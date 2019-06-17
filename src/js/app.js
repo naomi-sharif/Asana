59075ed36bcc7a39dc46b1efc4cb894f7ba5a573
Naomi <naomi@goldspecdigital.com>
2019-06-17 21:35:55 +0100

    Added function to toggle mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,26 @@ const app = {
 
   init() {
     this.toggleHamburger();
+    this.toggleMobileMenu();
   },
 
   toggleHamburger() {
     const hamburger = document.querySelector(".hamburger");
-    hamburger.addEventListener("click", function() {
+    hamburger.addEventListener("click", () => {
       hamburger.classList.toggle("active");
     })
-  }
+  },
+
+  toggleMobileMenu(){
+    const body = document.querySelector(".page-wrapper");
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const toggle = document.querySelector(".hamburger");
+    toggle.addEventListener("click", () => {
+      body.classList.toggle("page-wrapper--menu-open");
+      mobileMenu.classList.toggle("mobile-menu--open");
+    })
+  },
+
 }
 
 app.init();
